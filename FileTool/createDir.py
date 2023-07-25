84651d422ccd29ae3ded187bcb50db777bece2c7
llijiaqi <l@qq.com>
2023-07-25 17:38:50 +0800

    aa

---
+++ FileTool/createDir.py
@@ -0,0 +1,31 @@
+import os
+
+def get_next_out_directory():
+    i = 1
+    while True:
+        dir_name = "out{}".format(i)
+        if not os.path.exists(dir_name):
+            return dir_name
+        i += 1
+
+def create_folders_from_txt(txt_file):
+    out_directory = get_next_out_directory()
+
+    # 创建新的文件夹
+    os.makedirs(out_directory)
+
+    with open(txt_file, 'r') as file:
+        for line in file:
+            # 去除每行两边的空格和换行符
+            folder_name = line.strip()
+
+            # 在新的文件夹中创建子文件夹
+            folder_path = os.path.join(out_directory, folder_name)
+            os.makedirs(folder_path)
+
+            print("Created folder: {}".format(folder_path))
+
+if __name__ == "__main__":
+    # 请替换为你的txt文件路径
+    txt_file_path = "textfile1.txt"
+    create_folders_from_txt(txt_file_path)
---
+++ FileTool/createFileByTxt.py
@@ -0,0 +1,37 @@
+import os
+
+def get_next_out_directory():
+    i = 1
+    while True:
+        dir_name = "out{}".format(i)
+        if not os.path.exists(dir_name):
+            return dir_name
+        i += 1
+
+def create_files_from_txt(txt_file):
+    out_directory = get_next_out_directory()
+
+    # 创建out目录（如果不存在）
+    if not os.path.exists(out_directory):
+        os.makedirs(out_directory)
+
+    with open(txt_file, 'r') as file:
+        for line in file:
+            # 去除每行两边的空格和换行符
+            file_name = line.strip()
+
+            # 将".txt"后缀替换为".md"
+            file_name = os.path.splitext(file_name)[0] + ".md"
+
+            # 创建文件在out目录中
+            file_path = os.path.join(out_directory, file_name)
+            with open(file_path, 'w') as new_file:
+                # 可选：在每个文件中写入一些初始内容
+                new_file.write("This is a new file: {}".format(file_name))
+
+            print("Created file: {}".format(file_path))
+
+if __name__ == "__main__":
+    # 请替换为你的txt文件路径
+    txt_file_path = "textfile.txt"
+    create_files_from_txt(txt_file_path)
